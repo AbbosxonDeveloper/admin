5f29a58208a0594d95dfd66a9c47c18920832224
Akmaljon571 <ahmadjonovakmal079@gmail.com>
2023-01-19 22:54:52 +0500

    [+] Post in Food

--- src/modules/food/food.ts
+++ src/modules/food/food.ts
@@ -1,7 +1,8 @@
 import { NextFunction, Request, Response } from 'express';
+import { ErrorHandle } from '../../error/error';
 import { food } from '../../types/types';
 import postgres from '../../utils/postgres';
-import { dbFoodGet } from './model';
+import { dbFoodGet, dbFootPost } from './model';
 
 export const Get = async(req: Request, res: Response, next: NextFunction): Promise<void> => {
     try {
@@ -12,6 +13,30 @@ export const Get = async(req: Request, res: Response, next: NextFunction): Promi
             status: 200,
             data: allFood
         })
+    } catch (error) {
+        console.log(error)
+        next(error)
+    }
+}
+
+export const Post = async(req: Request, res: Response, next: NextFunction): Promise<void> => {
+    try {
+        const { title } = req.resolt
+
+        if (typeof title !== 'string') 
+            throw new ErrorHandle('Bad Request', 401)
+
+        const newFood: food = await postgres.fetchOne<food>(dbFootPost, title)
+
+        if (!newFood)
+            throw new ErrorHandle('Error in Database', 500)
+
+        res.json({
+            message: 'Created',
+            status: 201,
+            data: newFood
+        })
+        
     } catch (error) {
         console.log(error)
         next(error)
--- src/modules/food/model.ts
+++ src/modules/food/model.ts
@@ -1,3 +1,7 @@
 export const dbFoodGet = `
     SELECT * from food
 `
+
+export const dbFootPost = `
+    INSERT INTO food(title) values($1) RETURNING *
+`
